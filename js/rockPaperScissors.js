02bfdc7cd5bdde99b1fe3dfe6a042bf9183c4f2f
EdwardGoomba <ask@edwardvdesign.com>
2017-08-19 01:21:24 -0400

    Added functions to play game

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,41 +1,51 @@
-var userInput = '';
+var userInput = 'Scissors';
+
 userInput = userInput.toLowerCase();
 
 function getComputerChoice() {
-	Math.floor( Math.random() * 3 );
-	switch ( randomNumber ) {
-		case 0:
-			return 'rock';
-		case 1:
-			return 'paper';
-		case 2:
-			return 'scissors';
-	}
+  var randomNumber = Math.floor(Math.random() * 3);
+
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+                      }
 }
 
-function determineWinner( userChoice, computerChoice ) {
-	if ( userChoice === computerChoice ) {
-		return 'The game is a tie!';
-	}
-	if ( userChoice === 'rock' ) {
-		if ( computerChoice === 'paper' ) {
-			return 'The computer won!';
-		} else {
-			return 'You won!';
-		}
-	}
-	if ( userChoice === 'paper' ) {
-		if ( computerChoice === 'scissors' ) {
-			return 'The computer won!';
-		} else {
-			return 'You won!';
-		}
-	}
-	if ( userChoice === 'scissors' ) {
-		if ( computerChoice === 'rock' ) {
-			return 'The computer won!';
-		} else {
-			return 'You won!';
-		}
-	}
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return 'The game is a tie!';
+  } if (userChoice === 'rock') {
+    	if (computerChoice === 'paper') {
+      	return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  } if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  } if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
 }
+
+function playGame() {
+  var userChoice = userInput;
+  var computerChoice = getComputerChoice();
+  console.log('You picked: ' + userChoice);
+  console.log('The computer picked: ' + computerChoice);
+
+  console.log('And the winner is: ' + determineWinner(userChoice, computerChoice));
+}
+
+playGame();
